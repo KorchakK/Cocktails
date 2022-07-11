fb5df29788e93730ed97e68bc5afa6a5342264da
Korchak Konstantin <korchak.konstantin@gmail.com>
2022-07-11 21:16:00 +0300

    Initial Commit

---
+++ MVVM-C App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM-C App
+//
+//  Created by Konstantin Korchak on 11.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
